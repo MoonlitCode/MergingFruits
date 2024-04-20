9bb776a7599338298443b5df6b56095e05821d07
MoonlitMiguel <131905864+MoonlitMiguel@users.noreply.github.com>
2024-04-20 00:12:06 -0500

    Implement: Mouse Controls to 'FruitDropper.cs'

--- Scripts/Fruits/FruitDropper.cs
+++ Scripts/Fruits/FruitDropper.cs
@@ -1,83 +1,86 @@
-﻿#nullable enable
+#nullable enable
 using Godot;
 
 namespace MergingFruits.Scripts.Fruits;
 
 public partial class FruitDropper : Area2D {
-    [Export] private Node? _targetParent;
-    [Export] private Sprite2D? _dropperSprite;
-    [Export] private TextureRect? _fruitRect;
-    [Export] private CollisionShape2D? _collisionShape2D;
-    private FruitPackedWeightedList? _fruitPackedWeightedList;
+	[Export] private Node? _targetParent;
+	[Export] private Sprite2D? _dropperSprite;
+	[Export] private TextureRect? _fruitRect;
+	[Export] private CollisionShape2D? _collisionShape2D;
+	private FruitPackedWeightedList? _fruitPackedWeightedList;
 
-    public void Initialize(FruitPackedWeightedList fruitPackedWeightedList) {
-        _fruitPackedWeightedList = fruitPackedWeightedList;
-    }
+	public void Initialize(FruitPackedWeightedList fruitPackedWeightedList) {
+		_fruitPackedWeightedList = fruitPackedWeightedList;
+	}
 
-    //todo External inputs, move dropper, method for spawning fruit, include timer.
-    /// <summary>
-    /// Attempts to immediately reposition dropper
-    /// </summary>
-    /// <param name="position"></param>
-    public void TryRepositionDropper(Vector2I position) {
-        if (!HasAllComponents()) return;
-        
-    }
+	//todo External inputs, move dropper, method for spawning fruit, include timer.
+	/// <summary>
+	/// Attempts to immediately reposition dropper. To be used with KB+M control schemes.
+	/// </summary>
+	/// <param name="globalPosition"></param>
+	public void TryRepositionDropper(Vector2 globalPosition) {
+		if (!HasAllComponents()) return;
+		
+		var colliderSize = _collisionShape2D.Shape.GetRect().Size.X / 2;
+		if (!(Mathf.Abs(globalPosition.X) < colliderSize)) return;
+		_dropperSprite.GlobalPosition = new Vector2(globalPosition.X, _dropperSprite.GlobalPosition.Y);
+	}
 
-    /// <summary>
-    /// Attempts to translate dropper over time
-    /// </summary>
-    /// <param name="direction"></param>
-    public void TryTranslatingDropper(Vector2I direction) {
-        if (!HasAllComponents()) return;
-        
-    }
+	/// <summary>
+	/// Attempts to translate dropper over time. To be used with Gamepad control schemes.
+	/// </summary>
+	/// <param name="direction"></param>
+	public void TryTranslatingDropper(Vector2 direction) {
+		if (!HasAllComponents()) return;
+		
+	}
 
-    public void TrySpawnSpecificFruit(int index) {
-        if (!HasAllComponents()) return;
-        if (index > _fruitPackedWeightedList.Data.Count) index -= 1;
-        if (index < 0) index = 0;
+	public void TrySpawnSpecificFruit(int index) {
+		if (!HasAllComponents()) return;
+		if (index > _fruitPackedWeightedList.Data.Count) index -= 1;
+		if (index < 0) index = 0;
 
-        var targetPosition = _fruitRect.GlobalPosition + (_fruitRect.Size / 2);
-        FruitSpawner.RB2DInstOrNull(_targetParent, _fruitPackedWeightedList.Data[index].FruitScene, targetPosition);
-    }
+		var targetPosition = _fruitRect.GlobalPosition + (_fruitRect.Size / 2);
+		FruitSpawner.RB2DInstOrNull(_targetParent, _fruitPackedWeightedList.Data[index].FruitScene, targetPosition);
+	}
 
-    public void TrySpawnWeightedFruit() {
-        if (!HasAllComponents()) return;
-        
-        var fruitIndex = _fruitPackedWeightedList.Data.Count - 1;
-        
-        for (var i = fruitIndex; i >= 0; i--) {
-            var fruitWeight = _fruitPackedWeightedList.Data[i].Weight;
-            var randomRoll = GD.Randf();
-            if (fruitWeight >= randomRoll) {
-                GD.Print($"FruitWeight:{fruitWeight} | Roll:{randomRoll}");
-                TrySpawnSpecificFruit(i);
-                break;
-            }
-            //note Safety Check In-case Everything Is Rolled Past.
-            if (i <= 0) TrySpawnSpecificFruit(0);
-        }
-    }
+	public void TrySpawnWeightedFruit() {
+		if (!HasAllComponents()) return;
+		
+		var fruitIndex = _fruitPackedWeightedList.Data.Count - 1;
+		
+		for (var i = fruitIndex; i >= 0; i--) {
+			var fruitWeight = _fruitPackedWeightedList.Data[i].Weight;
+			var randomRoll = GD.Randf();
+			if (fruitWeight >= randomRoll) {
+				GD.Print($"FruitWeight:{fruitWeight} | Roll:{randomRoll}");
+				TrySpawnSpecificFruit(i);
+				break;
+			}
+			//note Safety Check In-case Everything Is Rolled Past.
+			if (i <= 0) TrySpawnSpecificFruit(0);
+		}
+	}
 
-    private bool HasAllComponents() {
-        if (_dropperSprite is null) {
-            GD.PrintErr($"FruitDropper.cs is Missing: _dropperSprite");
-            return false;
-        }
-        if (_targetParent is null) {
-            GD.PrintErr($"FruitDropper.cs is Missing: _targetParent");
-            return false;
-        }
-        if (_fruitRect is null) {
-            GD.PrintErr($"FruitDropper.cs is Missing: _fruitRect");
-            return false;
-        }
-        if (_collisionShape2D is null) {
-            GD.PrintErr($"FruitDropper.cs is Missing: _collisionShape2D");
-            return false;
-        }
+	private bool HasAllComponents() {
+		if (_dropperSprite is null) {
+			GD.PrintErr($"FruitDropper.cs is Missing: _dropperSprite");
+			return false;
+		}
+		if (_targetParent is null) {
+			GD.PrintErr($"FruitDropper.cs is Missing: _targetParent");
+			return false;
+		}
+		if (_fruitRect is null) {
+			GD.PrintErr($"FruitDropper.cs is Missing: _fruitRect");
+			return false;
+		}
+		if (_collisionShape2D is null) {
+			GD.PrintErr($"FruitDropper.cs is Missing: _collisionShape2D");
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -6,52 +6,60 @@ namespace MergingFruits.Scripts;
 
 public partial class Game : Node {
 	[Export] private FruitDropper? _fruitDropper;
-    
-    [Export] private FruitPackedList? _fruitPackedList;
-    [Export] private FruitPackedWeightedList? _fruitPackedWeightedList;
-    [Export] private Node? _fruitBasket;
-    
-    public override void _Ready() {
-        base._Ready();
-        if (!HasAllComponents()) return;
+	
+	[Export] private FruitPackedList? _fruitPackedList;
+	[Export] private FruitPackedWeightedList? _fruitPackedWeightedList;
+	[Export] private Node? _fruitBasket;
+	
+	public override void _Ready() {
+		base._Ready();
+		if (!HasAllComponents()) return;
 
-        _fruitDropper.Initialize(_fruitPackedWeightedList);
-        FruitMerger.InitFruitMerger(_fruitBasket, _fruitPackedList);
-        Fruit.OnSameFruitTierCollision += Fruit_OnSameFruitTierCollision;
-    }
+		_fruitDropper.Initialize(_fruitPackedWeightedList);
+		FruitMerger.InitFruitMerger(_fruitBasket, _fruitPackedList);
+		Fruit.OnSameFruitTierCollision += Fruit_OnSameFruitTierCollision;
+	}
 
-    public override void _Process(double delta) {
-        base._Process(delta);
-        if (!HasAllComponents()) return;
+	public override void _Process(double delta) {
+		base._Process(delta);
+		if (!HasAllComponents()) return;
+		CalculateMouseControls();
+	}
 
-        if (Input.IsActionJustPressed(StringInputs.DropFruit)) {
-            _fruitDropper.TrySpawnWeightedFruit();
-        }
-    }
-
-    private void Fruit_OnSameFruitTierCollision(object? sender, FruitPair e) {
-        if (e.Fruit1.IsAttemptingMerge || e.Fruit2.IsAttemptingMerge) return;
-        e.Fruit1.IsAttemptingMerge = true;
-        e.Fruit2.IsAttemptingMerge = true;
-        FruitMerger.ProcessMerge(e);
-    }
-
-    private bool HasAllComponents() {
-        if (_fruitDropper is null) {
-            GD.PrintErr($"Game.cs is Missing: _fruitDropper");
-            return false;
-        }
+	private void CalculateMouseControls() {
+		var mousePosition = GetWindow().GetMousePosition();
+		var halfWindowSize = (Vector2)(GetWindow().Size / 2);
+		var centerOriginPosition = mousePosition - halfWindowSize ;
 		
-        if (_fruitPackedList is null) {
-            GD.PrintErr($"Game.cs is Missing: _packedFruitList");
-            return false;
-        }
-        
-        if (_fruitBasket is null) {
-            GD.PrintErr($"Game.cs is Missing: _fruitBasket");
-            return false;
-        }
+		_fruitDropper.TryRepositionDropper(centerOriginPosition);
+		if (Input.IsActionJustPressed(StringInputs.DropFruit)) {
+			_fruitDropper.TrySpawnWeightedFruit();
+		}
+	}
 
-        return true;
-    }
+	private void Fruit_OnSameFruitTierCollision(object? sender, FruitPair e) {
+		if (e.Fruit1.IsAttemptingMerge || e.Fruit2.IsAttemptingMerge) return;
+		e.Fruit1.IsAttemptingMerge = true;
+		e.Fruit2.IsAttemptingMerge = true;
+		FruitMerger.ProcessMerge(e);
+	}
+
+	private bool HasAllComponents() {
+		if (_fruitDropper is null) {
+			GD.PrintErr($"Game.cs is Missing: _fruitDropper");
+			return false;
+		}
+		
+		if (_fruitPackedList is null) {
+			GD.PrintErr($"Game.cs is Missing: _packedFruitList");
+			return false;
+		}
+		
+		if (_fruitBasket is null) {
+			GD.PrintErr($"Game.cs is Missing: _fruitBasket");
+			return false;
+		}
+
+		return true;
+	}
 }
