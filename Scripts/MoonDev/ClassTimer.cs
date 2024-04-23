b6d03f7f94fda0614cda452eca4bf6167cde556f
MoonlitMiguel <131905864+MoonlitMiguel@users.noreply.github.com>
2024-04-22 21:12:23 -0500

    Add: "Physics Material" for "Fruit"

--- Scripts/MoonDev/ClassTimer.cs
+++ Scripts/MoonDev/ClassTimer.cs
@@ -12,26 +12,53 @@ public class ClassTimer {
     public float CurrentTimer => _currentTimer;
     public bool HasTimerEnded => _hasTimerEnded;
 
+    /// <summary>
+    /// Used to display on a "Progress bar".
+    /// </summary>
+    /// <returns>Value goes from 1 to 0</returns>
     public float TimerNormalizedDecreasing() {
         if (_currentTimer == 0) return 0;
         return _currentTimer / MaxTimer;
     }
 
+    /// <summary>
+    /// Used to display on a "Progress bar".
+    /// </summary>
+    /// <returns>Value goes from 0 to 1</returns>
     public float TimerNormalizedIncreasing() {
         var timerValue = 1 - TimerNormalizedDecreasing();
         if (timerValue > 1) return 1;
         return timerValue;
     }
 
+    /// <summary>
+    /// Sets the 'MaxTimer'
+    /// </summary>
+    /// <param name="maxTimer"></param>
     public void InitializeTimer(float maxTimer) {
         MaxTimer = maxTimer;
     }
 
+    /// <summary>
+    /// Sets the "Current timer" to 'MaxTimer'.
+    /// </summary>
     public void ResetTimer() {
         _currentTimer = MaxTimer;
         _hasTimerEnded = false;
     }
 
+    /// <summary>
+    /// Stops the "Current Timer"
+    /// </summary>
+    public void StopTimer() {
+        _currentTimer = 0;
+        _hasTimerEnded = true;
+    }
+    
+    /// <summary>
+    /// Increments the "Current timer".
+    /// </summary>
+    /// <param name="amount">Uses "Delta" time typically.</param>
     public void IncrementCurrentTimer(float amount) {
         _currentTimer += amount;
         
@@ -39,6 +66,10 @@ public class ClassTimer {
         _currentTimer = MaxTimer;
     }
 
+    /// <summary>
+    /// Decrements the "Current timer".
+    /// </summary>
+    /// <param name="amount">Uses "Delta" time typically.</param>
     public void DecrementCurrentTimer(float amount) {
         _currentTimer -= amount;
         
